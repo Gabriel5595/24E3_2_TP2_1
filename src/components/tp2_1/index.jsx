962a5cad11bcd343bca0518da864be55707ff575
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-09-01 12:23:56 -0300

    Finalização do Projeto

---
+++ src/components/tp2_1/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+export default function TP2_1() {
+    return (
+        <div className="TP2_1_box">
+            <img src="https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dwf2a76f3a/images/full/full_2024_/2022/full_2024_3790_ultimate-cf-sl-7_P03_P5.jpg?sw=1300&sfrm=png&q=90&bgcolor=F2F2F2" alt="Canyon Bike" />
+            <h1>Ultimate CF SL 7</h1>
+            <h4>U$ 2,069,00</h4>
+            <p>A nova Ultimate atinge o equilíbrio perfeito entre leveza, rigidez, aerodinâmica, conforto e robustez. O resultado? A bicicleta de estrada mais equilibrada e de melhor valor no mercado.</p>
+        </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import TP2_1 from "./components/tp2_1/index"
+
+createRoot(document.getElementById('root')).render(
+  <TP2_1 />
+)
